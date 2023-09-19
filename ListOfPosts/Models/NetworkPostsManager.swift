ab27704e3f9ab335ec90a21d5f0ab5ecfe9e0bbc
Andrii Stetsenko <andruxa7@ukr.net>
2023-09-20 01:14:37 +0300

    add NetworkPostsManager

---
+++ ListOfPosts/Models/NetworkPostsManager.swift
@@ -0,0 +1,45 @@
+//
+//  NetworkPostsManager.swift
+//  ListOfPosts
+//
+//  Created by Andrii Stetsenko on 20.09.2023.
+//
+
+import Foundation
+
+struct NetworkPostsManager {
+    
+    var onCompletionPostsData: ((Post) -> Void)?
+    
+    func getPost() {
+        let urlString = "https://raw.githubusercontent.com/anton-natife/jsons/master/api/main.json"
+        getURL(string: urlString)
+    }
+    
+    func getURL(string: String) {
+        guard let url = URL(string: string) else { return }
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { data, response, error in
+            if let data = data {
+                if let currentData = self.parsePostJSON(withData: data) {
+                    self.onCompletionPostsData?(currentData)
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    func parsePostJSON(withData data: Data) -> Post? {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .secondsSince1970
+        
+        do {
+            let decodedPostData = try decoder.decode(Post.self, from: data)
+            return decodedPostData
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        return nil
+    }
+    
+}
